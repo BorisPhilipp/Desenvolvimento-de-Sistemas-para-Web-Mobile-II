595eef2d1c2bb78a56f541eb89a792543e37a7d5
Boris Philipp <129879125+BorisPhilipp@users.noreply.github.com>
2024-10-01 08:05:38 -0300

    Update server.js

--- aula7 node/server.js
+++ aula7 node/server.js
@@ -1,12 +1,24 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path'); //Primeiro desafio
 
 const requestListener = function (req, res) {
     if(req.url === "/"){
         res.writeHead(200);
         res.end("Você está na Página Inicial");
     } else if(req.url === "/sobre"){
-        res.writeHead(200);
-        res.end("Você está na página sobre")
+        const filePath = path.join(__dirname, 'sobre.txt'); //Desafio
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                res.writeHead(200);
+                res.end("Página não encontrada.");
+                return;
+            }
+            res.writeHead(200);
+            res.end(data);
+        })
+
     }else{
         res.writeHead(404);
         res.end("Página não encontrada");
