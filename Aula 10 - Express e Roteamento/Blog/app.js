f95c34d8455cfe270a979b65140466e231260f33
Boris Philipp <129879125+BorisPhilipp@users.noreply.github.com>
2024-10-09 22:09:38 -0300

    Add files via upload

--- Aula 10 - Express e Roteamento/Blog/app.js
+++ Aula 10 - Express e Roteamento/Blog/app.js
@@ -15,4 +15,52 @@ app.use(express.static('public'));
 //Configurar o processamento de dados do forms
 app.use(bodyParser.urlencoded({extended: true}));
 
+//Dados
+const posts = [
+    {
+        id: 1,
+        titulo: 'Primeira Postagem',
+        conteudo: 'Esse é o conteudo da Primeira Postagem'
+    },
+    {
+        id: 2,
+        titulo: 'Segunda Postagem',
+        conteudo: 'Esse é o conteudo da Segunda Postagem'
+    }
+];
+
+//rota principal
+app.get('/', (req, res)=>{
+    res.render('index', { posts });
+});
+
+//rota para exibir os detalhes de um post
+app.get('/post/:id',(req, res)=> {
+    const id = req.params.id;
+    const post = posts.find(post => post.id === parseInt(id));
+    res.render('post', { post });
+});
+
+//rota para exibir o formulario de add
+app.get('/add', (req,res) => {
+    res.render('add');
+});
+
+//rota para processar os dados do forms de add
+app.post('/add', (req,res)=>{
+    const { titulo, conteudo } = req.body;
+    const id = posts.length + 1;
+
+    posts.push({ id, titulo, conteudo });
+    res.redirect('/');
+});
+
+//subir o servidor
+app.listen(porta, ()=>{
+    console.log(`Servidor aberto em http://localhost:${porta}`);
+})
+
+
+
+
 
